7466d4637f1cf8c9c5b3f6b0860e728cc35253b8
jcool12 <jjccooooll12@gmail.com>
2020-03-11 16:50:09 +0100

    first commit

---
+++ api/views.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.contrib.auth.models import User
+from .models import Image, Rating
+from .serializers import UserSerializer, ImageSerializer, RatingSerializer
+from django.contrib.auth.models import User
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+# Create your views here.
+
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (AllowAny, )
+
+    @action(detail=True, methods=['POST'])
+    def rate_image(self,request,pk=None):
+        if 'stars' in request.data:
+            stars = request.data['stars']
+            user = request.user
+            image = Image.objects.get(id=pk)
+
+            try:
+                #image & user are foreign keys in the model, so that we need to specify
+                #into the Rating object which are these parameters
+                rating = Rating.objects.get(user=user.id, image=image.id)
+                rating.stars = stars
+                rating.save()
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message':'Rating updated', 'result':serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+            except:
+                rating= Rating.objects.create(user=user, image=image, stars=stars)
+                serializer = RatingSerializer(rating, many=False)
+                response = {'message':'Rating created', 'result':serializer.data}
+                return Response(response, status=status.HTTP_200_OK)
+        else:
+            return Response({'message':'something went wrong'}, status=status.HTTP_400_BAD_REQUEST)
+ 
+
+
+class RatingViewSet(viewsets.ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (AllowAny, )
+
+    #Since we already created our own method rate_movie to update/create we make the
+    #built-in Viewset one uneffective by overriding it
+    def update(self,request,*args, **kwargs):
+        response = {'message': 'You cannot update rating like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+    
+    def create(self,request,*args, **kwargs):
+        response = {'message': 'You cannot create rating like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    authentication_classes = (TokenAuthentication, ) 
+    permission_classes = (AllowAny, )
+
+
+
+
+
+
+
+
+ 
+
+    
---
+++ imageRater/settings.py
@@ -0,0 +1,157 @@
+
+import os
+from decouple import config
+from dj_database_url import parse as dburl 
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG')
+
+ALLOWED_HOSTS = ['my-image-rater.herokuapp.com', '127.0.0.1']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'api',
+    'corsheaders',  
+    'rest_framework',
+    'rest_framework.authtoken', 
+    'imagekit',
+    'storages'
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'imageRater.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'imageRater.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+default_dburl =  'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = {
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
+    }
+
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
+
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
+MEDIA_URL = '/media/'
+
+CORS_ORIGIN_WHITELIST = [ 
+
+    'http://localhost:3000', 
+
+    'http://localhost:8000', 
+
+    'http://localhost:8080', 
+
+    'https://image-rater.web.app',
+
+    'https://image-rater.firebaseapp.com'
+   
+] 
+
+
+AWS_ACCESS_KEY_ID= config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY= config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME= config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_ENDPOINT_URL='https://s3-eu-west-3.amazonaws.com'
+AWS_S3_REGION_NAME='eu-west-3'
+     
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
+
